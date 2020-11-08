7099be8b5e8088359c4506b2ce4d6ef3d0561b53
John-Arne Fagerli <fagerli@gmail.com>
2020-11-08 20:49:29 +0100

    added decrement on score in cases of error touch

--- src/Score.js
+++ src/Score.js
@@ -8,16 +8,26 @@ class Score extends React.Component {
       score: 0
     }
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
   increment(){
     this.setState({score : this.state.score+1});
   }
+
+  decrement(){
+    if(this.state.score > 0){
+      this.setState({score : this.state.score-1});
+    }
+  }
   
   render() {
     return(
-      <span className="score" onClick={this.increment}>
+      <span className="scoreContainer">
+      <div className="score" onClick={this.increment}>
       {this.state.score}
+      </div>
+      <div onClick={this.decrement}>-</div>
       </span>
     );
   }
